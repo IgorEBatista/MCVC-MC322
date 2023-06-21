a85f182dec214e3dc7979bff0077c3b0d9412495
Gabi Jacob <g186087@dac.unicamp.com>
2023-06-21 19:55:51 -0300

    Atualizacao no atributo peso

--- src/AviaoCargueiro.java
+++ src/AviaoCargueiro.java
@@ -1,11 +1,13 @@
 public class AviaoCargueiro extends Aviao{
     
     String tipoCarga;
+    double pesoAtual;
 
     public AviaoCargueiro(String serie, String modelo, String marca, Aeroporto local, double capacidade, double combustivel,
-            double consumoCombustivel, double envergadura, String tipoCarga) {
+            double consumoCombustivel, double envergadura, String tipoCarga, double pesoAtual) {
         super(serie, modelo, marca, local, capacidade, combustivel, consumoCombustivel, envergadura);
         this.tipoCarga = tipoCarga;
+        this.pesoAtual = pesoAtual;
     }
 
     @Override
@@ -34,8 +36,15 @@ public class AviaoCargueiro extends Aviao{
         this.tipoCarga = tipoCarga;
     }
 
-    public String toString(){
-        
+    public double getPesoAtual() {
+        return pesoAtual;
     }
 
+    public void setPesoAtual(double pesoAtual) {
+        this.pesoAtual = pesoAtual;
+    }
+
+    public String toString(){
+        return "Número de série: " + this.serie + "\nModelo: " + this.modelo + "\nLocal atual: " + this.local + "\nCapacidade: " + this.capacidade + "\nNível de combustível atual: " + this.combustivel + "\nConsumo de combustível(l/km): " + this.consumoCombustivel + "\nEnvergadura(m): " + this.envergadura + "\nTipo de carga: " + this.tipoCarga + "\nPeso atual: " + this.pesoAtual;
+    }
 }
