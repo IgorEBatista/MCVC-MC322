a1356f0027e9b243e5fabc474e61369e3ec662d2
leticialopesms <l184423@dac.unicamp.br>
2023-06-22 18:20:44 -0300

    Talvez é possível que a gente tenha resolvido o algoritmo de grafos

--- src/Companhia.java
+++ src/Companhia.java
@@ -149,13 +149,13 @@ public class Companhia {
         return false;
     }
 
-    public Aviao buscarAviao(String serie) {
+    public Aviao buscarAviao(int serie) {
         /* Busca, na lista de aviões, o avião que tem o número de série
         dado como parâmetro.
         Retorna o avião se ele estiver na lista.
         Caso contrário, retorna null. */
         for (Aviao a: listaAvioes) 
-            if (a.getSerie().equals(serie))
+            if (a.gerarSerie() == serie)
                 return a;
         return null;
     }
@@ -305,10 +305,22 @@ public class Companhia {
             Aeroporto atual = pilha.pop();
 
             if(!foiVisitado.get(atual)){
-                foiVisitado.put(atual, true);
+
+                boolean check = false;
+                for (Voo visit : atual.getListaVoos()){
+                    if (!foiVisitado.get(visit.getDestino())){
+                        check = true;
+                    }
+                }
+
+                if (atual.getListaVoos().size() == 0 || check == false){
+                    foiVisitado.put(atual, true);
+                }
+
 
                 if (atual.equals(destino)){
-                    System.out.println(foiVisitado);
+                    System.out.println("oi");
+                    foiVisitado.put(atual, false);
                 }
 
                 for (Voo adj : atual.getListaVoos()){
