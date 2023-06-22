1452b2ea202ffbe4578729ecf8168ad5e34b2629
Gabi Jacob <g186087@dac.unicamp.com>
2023-06-22 15:09:58 -0300

    alteracao no pesoAtual

--- src/AviaoCargueiro.java
+++ src/AviaoCargueiro.java
@@ -1,13 +1,11 @@
 public class AviaoCargueiro extends Aviao{
     
-    String tipoCarga;
-    double pesoAtual;
+    private String tipoCarga;
 
     public AviaoCargueiro(String serie, String modelo, String marca, Aeroporto local, double capacidade, double combustivel,
             double consumoCombustivel, double envergadura, String tipoCarga, double pesoAtual) {
-        super(serie, modelo, marca, local, capacidade, combustivel, consumoCombustivel, envergadura);
+        super(serie, modelo, marca, local, capacidade, consumoCombustivel, envergadura, pesoAtual);
         this.tipoCarga = tipoCarga;
-        this.pesoAtual = pesoAtual;
     }
 
     public String getTipoCarga() {
@@ -18,18 +16,7 @@ public class AviaoCargueiro extends Aviao{
         this.tipoCarga = tipoCarga;
     }
 
-    public double getPesoAtual() {
-        return pesoAtual;
-    }
-
-    public void setPesoAtual(double pesoAtual) {
-        this.pesoAtual = pesoAtual;
-    }
-
     public String toString(){
-        return "Número de série: " + this.serie + "\nModelo: " + this.modelo + "\nLocal atual: " + this.local + 
-        "\nCapacidade: " + this.capacidade + "\nNível de combustível atual: " + this.combustivel + 
-        "\nConsumo de combustível(l/km): " + this.consumoCombustivel + "\nEnvergadura(m): " + this.envergadura + 
-        "\nTipo de carga: " + this.tipoCarga + "\nPeso atual: " + this.pesoAtual;
+        return super.toString() + "\nTipo de carga: " + this.tipoCarga;
     }
 }
