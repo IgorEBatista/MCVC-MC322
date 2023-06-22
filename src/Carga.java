86693d767e11be80af97bf57accc9862df168323
Gabi Jacob <g186087@dac.unicamp.com>
2023-06-22 15:45:26 -0300

    criacao da classe carga

--- src/Carga.java
+++ src/Carga.java
@@ -5,9 +5,51 @@ public class Carga {
     private double peso;
     private Passagem passagem;
 
-    public Carga(String tipoCarga, doule peso, Passagem passagem){
+    public Carga(String tipoCarga, double peso, Passagem passagem){
         this.ID = gerarID();
+        this.tipoCarga = tipoCarga;
+        this.peso = peso;
+        this.passagem = passagem;
+    }
 
+    //Métodos
+    public int gerarID() {
+        /* Gera um número a partir do atributo de classe 'registros'.
+        o ID gerado tem, pelo menos, 7 dígitos. */
+        registros++;
+        return registros;
+    }
+
+    //getters e setters
+    public int getID() {
+        return ID;
+    }
+    
+    public String getTipoCarga() {
+        return tipoCarga;
+    }
+
+    public void setTipoCarga(String tipoCarga) {
+        this.tipoCarga = tipoCarga;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public Passagem getPassagem() {
+        return passagem;
+    }
+
+    public void setPassagem(Passagem passagem) {
+        this.passagem = passagem;
+    }
+
+    public String toString(){
 
     }
 
