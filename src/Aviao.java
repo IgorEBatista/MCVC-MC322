e164796eebea95dac811a9bf50466b2924bea0bf
Gabi Jacob <g186087@dac.unicamp.com>
2023-06-22 15:09:58 -0300

    alteracao no pesoAtual

--- src/Aviao.java
+++ src/Aviao.java
@@ -9,17 +9,19 @@ abstract class Aviao{
     private double combustivel;
     private double consumoCombustivel;
     private double envergadura;
+    private double pesoAtual;
 
-    public Aviao(String serie, String modelo, String marca, Aeroporto local, double capacidade, double combustivel,
-            double consumoCombustivel, double envergadura) {
+    public Aviao(String serie, String modelo, String marca, Aeroporto local, double capacidade,
+            double consumoCombustivel, double envergadura, double pesoAtual) {
         this.serie = gerarSerie();
         this.modelo = modelo;
         this.marca = marca;
         this.local = local;
         this.capacidade = capacidade;
-        this.combustivel = combustivel;
+        this.combustivel = capacidade;
         this.consumoCombustivel = consumoCombustivel;
         this.envergadura = envergadura;
+        this.pesoAtual = pesoAtual;
     }
 
     public double calculaAlcance(){
@@ -102,8 +104,16 @@ abstract class Aviao{
         registros++;
         return registros;
     }
+    
+    public double getPesoAtual() {
+        return pesoAtual;
+    }
+
+    public void setPesoAtual(double pesoAtual) {
+        this.pesoAtual = pesoAtual;
+    }
 
     public String toString(){
-        return "Número de série: " + this.serie + "\nModelo: " + this.modelo + "\nLocal atual: " + this.local + "\nCapacidade: " + this.capacidade + "\nNível de combustível atual: " + this.combustivel + "\nConsumo de combustível(l/km): " + this.consumoCombustivel + "\nEnvergadura(m): " + this.envergadura;
+        return "Número de série: " + this.serie + "\nModelo: " + this.modelo + "\nLocal atual: " + this.local + "\nCapacidade: " + this.capacidade + "\nNível de combustível atual: " + this.combustivel + "\nConsumo de combustível(l/km): " + this.consumoCombustivel + "\nEnvergadura(m): " + this.envergadura + "\nPeso atual: " + this.pesoAtual;
     }
 }
--- src/AviaoComercial.java
+++ src/AviaoComercial.java
@@ -3,13 +3,11 @@ import java.util.ArrayList;
 public class AviaoComercial extends Aviao {
     
     private int passageirosMax;
-    private ArrayList<Passageiro> listaPassageiros;
 
     public AviaoComercial(String serie, String modelo, String marca, Aeroporto local, double capacidade, double combustivel,
-            double consumoCombustivel, double envergadura, int passageirosMax, ArrayList<Passageiro> listaPassageiros) {
-        super(serie, modelo, marca, local, capacidade, combustivel, consumoCombustivel, envergadura);
+            double consumoCombustivel, double envergadura, int passageirosMax) {
+        super(serie, modelo, marca, local, capacidade, consumoCombustivel, envergadura);
         this.passageirosMax = passageirosMax;
-        this.listaPassageiros = listaPassageiros;
     }
 
     @Override
@@ -18,7 +16,7 @@ public class AviaoComercial extends Aviao {
         for(Passageiro passageiro_iterado : voo.getListaPassageiros()){
             peso_entrando  += (passageiro_iterado.getPesoBagagem() + 70);
         }
-        if (peso_entrando <= capacidade){
+        if (peso_entrando <= getCombustivel()){
             setPesoAtual(peso_entrando); 
             return true;
         }
@@ -59,6 +57,6 @@ public class AviaoComercial extends Aviao {
         for(Passageiro passageiro_iterado : listaPassageiros){
             listaPassageiros_str = listaPassageiros_str +  passageiro_iterado.getNome() + "\n";
         }
-        return "Número de série: " + this.serie + "\nModelo: " + this.modelo + "\nLocal atual: " + this.local + "\nCapacidade: " + this.capacidade + "\nNível de combustível atual: " + this.combustivel + "\nConsumo de combustível(l/km): " + this.consumoCombustivel + "\nEnvergadura(m): " + this.envergadura + "\nNúmero máximo de passageiros: " + this.passageirosMax + "\nLista de passageiros: " + listaPassageiros_str;
+        return super.toString() + "\nNúmero máximo de passageiros: " + this.passageirosMax + "\nLista de passageiros: " + listaPassageiros_str;
     }
 }
