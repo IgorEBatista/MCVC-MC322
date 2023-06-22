9cf6f3007e377e54f7130ad4560552798ab72f10
Igor Batista <igorengbatista@gmail.com>
2023-06-22 17:05:50 -0300

    implementacao de calculaPreco

--- src/Trajeto.java
+++ src/Trajeto.java
@@ -4,19 +4,23 @@ public class Trajeto {
     // Atributos (Propriedades)
     private ArrayList<Voo> rota;   // Lista de conexões que compoem o trajeto
     private double distanciaTotal;
-    
+   
     // Construtor
     public Trajeto() {
         this.rota = new ArrayList<Voo>();
-        this.distanciaTotal = getDistanciaTotal();
+        this.distanciaTotal = calculaDistanciaTotal();
     }
 
     // Métodos
-    public double getDistanciaTotal() {
-        return distanciaTotal;
-    }
 
     // - Getters e Setters
+    public double getDistanciaTotal() {
+        return distanciaTotal;
+    } 
+
+    public void setDistanciaTotal(double distanciaTotal) {
+        this.distanciaTotal = distanciaTotal;
+    }
 
     public ArrayList<Voo> getRota() {
         return rota;
@@ -31,6 +35,7 @@ public class Trajeto {
     public boolean adicionarRota(Voo voo) {
         /* Adiciona uma conexão à rota. */
         rota.add(voo);
+        atualizaDistancia();
         return true;
     }
 
@@ -39,6 +44,7 @@ public class Trajeto {
         Se  */
         if (rota.contains(voo)) {
             rota.remove(voo);
+            atualizaDistancia();
             return true;
         }
         return false;
@@ -51,6 +57,18 @@ public class Trajeto {
             lista += (v.getOrigem()).getNome() + " -> " + (v.getDestino()).getNome() + "\n";
         }
         return lista;
+    }
+
+    public double calculaDistanciaTotal() {
+        double distanciaTot = 0.0;
+        for (Voo voo : rota) {
+            distanciaTot += Coordenada.calculaDistancia(voo.getOrigem(), voo.getDestino());
+        }
+        return distanciaTot;
+    }
+
+    public void atualizaDistancia() {
+        setDistanciaTotal(calculaDistanciaTotal());
     }
 
     @Override
