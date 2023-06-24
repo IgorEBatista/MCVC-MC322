2bcf50cc6e4a19708d936311c1fd026b8d36eb85
Gabi Jacob <g186087@dac.unicamp.com>
2023-06-24 15:41:08 -0300

    modificacoes na parte grafica

--- src/Swing.java
+++ src/Swing.java
@@ -2,23 +2,26 @@ import javax.swing.*;
 public class Swing {  
     public static void main(String[] args) { 
         //criando uma instancia da frame que vai exibir os botões 
-        JFrame frame = new JFrame();
+        JFrame frame = new JFrame("MCVC");
 
         //Criando os botões                
         JButton cadastrar = new JButton("Cadastrar");
-        JButton remover = new JButton("Remover");  
-        JButton sair = new JButton("Sair");  
-        
+        //TODO Fazer uma lista de botoes com todas as cidades pra pessoa clicar em qual ela quer remover
+        JButton remover = new JButton("Remover");
+        JButton simular_compra = new JButton("Simular compra");
+        JButton sair = new JButton("Sair");
 
         //Setando os tamanhos dos botões
-        cadastrar.setBounds(130,100,100, 40);
-        remover.setBounds(130,200,100, 40); 
-        sair.setBounds(130,300,100, 40); 
+        cadastrar.setBounds(100,100,170, 40);
+        remover.setBounds(100,170,170, 40); 
+        simular_compra.setBounds(100,240,170, 40);
+        sair.setBounds(100, 310, 170, 40); 
 
         //Adicionando o botão na frame (JFrame)
         frame.add(cadastrar);
         frame.add(remover);
         frame.add(sair);
+        frame.add(simular_compra);
         
         //Setando as características da frame     
         frame.setSize(400,500);//400 width and 500 height  
