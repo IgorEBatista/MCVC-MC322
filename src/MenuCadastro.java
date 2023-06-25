ab26b9dc8a39b9667eaa1eee3668f68fc9c9b01d
Igor Batista <105023521+IgorEBatista@users.noreply.github.com>
2023-06-25 12:05:55 -0300

    inicio do menu cadastro

--- src/MenuCadastro.java
+++ src/MenuCadastro.java
@@ -1,3 +1,27 @@
-public class MenuCadastro {
+public abstract class MenuCadastro {
     //TODO implementar
+
+    //Métodos
+    public static boolean cadastrarAeroporto(Companhia companhia){
+        //TODO implementar
+        Aeroporto aeroporto;
+        Coordenada coordenada;
+        double lat, lon, largura;
+        String nome, cidade;
+
+        
+        return false;
+    }
+    public static boolean cadastrarAviao(Companhia companhia){
+        //TODO implementar
+        return false;
+    }
+    public static boolean cadastrarCliente(Companhia companhia){
+        //TODO implementar
+        return false;
+    }
+    public static boolean cadastrarVoo(Companhia companhia){
+        //TODO implementar
+        return false;
+    }
 }
