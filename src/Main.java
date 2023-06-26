7eb5154e7e3a734118919419817ce80585f38425
Igor Batista <105023521+IgorEBatista@users.noreply.github.com>
2023-06-26 04:13:59 -0300

    correção do caminho do arquivo

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main {
                                   "Companhia",
                                   3);
 
-        File entradas = new File("C:\\Users\\igore\\Documents\\Unicamp\\2023 - S1\\MC322\\Grupo-MCVC\\MCVC-MC322\\src\\entrada.txt");
+        File entradas = new File(".\\src\\entrada.txt");
         Leitura.lerArquivo(entradas);
 
         Coordenada coordenada1 = new Coordenada(-23.4322, -46.4692);
